b136ee32ae8de34a8667bb7dac7c73e724d1124c
Carlo-Adile <adile.carlo.a@gmailcom>
2024-03-13 18:06:30 +0100

    add function to edit name

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,11 @@ const app = Vue.createApp({
           complete: false,
         }
       ],
-      newToDoTitle: ''
+      /* save new to do title */
+      newToDoTitle: '',
+      /* modify to do */
+      editedToDoTitle: '',
+      editedToDoId: null,
     };
   },
   methods: {
@@ -18,10 +22,28 @@ const app = Vue.createApp({
         title: this.newToDoTitle,
         completed: false,
       });
+      /* reset */
       this.newToDoTitle = '';
     },
     removeToDo(id){
+      /* pass a new array without the to do with this id */
       this.toDoList = this.toDoList.filter(todo => todo.id !== id);
+    },
+    editToDo(id, title){
+      /* change template to input text */
+      this.editedToDoId = id;
+      this.editedToDoTitle = title;
+    },
+    editDone(){
+      /* iterate to find id to edit */
+      this.toDoList.forEach(todo => {
+        if (todo.id === this.editedToDoId) {
+          todo.title = this.editedToDoTitle;
+        }
+      });
+      /* reset */
+      this.editedToDoId = null;
+      this.editedToDoTitle = '';
     }
   }
 });
